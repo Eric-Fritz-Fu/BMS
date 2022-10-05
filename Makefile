0f6e419adc830e302e5b66239db4f2326f08f80e
Eric-Fritz-Fu <78291982+Eric-Fritz-Fu@users.noreply.github.com>
2022-10-05 22:50:52 +0800

    Move getch() to common.c

--- Makefile
+++ Makefile
@@ -1,2 +1,5 @@
 database.o: database.c
 	gcc database.c -c
+
+common.o: common.c
+	gcc common.o -c
--- database.c
+++ database.c
@@ -1,37 +1,7 @@
-#include <assert.h>
-#include <stdlib.h>
-#include <string.h>
-#include <termios.h>
-#include <unistd.h>
-
 #include <stdio.h>
+#include <stdlib.h>
 
 #define DATABASE "database"
-
-/*
- * Copy from CSDN
- *
- *    https://blog.csdn.net/ky_heart/article/details/53783479
- *
- */
-int getch() {
-  int c = 0;
-  struct termios org_opts, new_opts;
-  int res = 0;
-  //-----  store old settings -----------
-  res = tcgetattr(STDIN_FILENO, &org_opts);
-  assert(res == 0);
-  //---- set new terminal parms --------
-  memcpy(&new_opts, &org_opts, sizeof(new_opts));
-  new_opts.c_lflag &=
-      ~(ICANON | ECHO | ECHOE | ECHOK | ECHONL | ECHOPRT | ECHOKE | ICRNL);
-  tcsetattr(STDIN_FILENO, TCSANOW, &new_opts);
-  c = getchar();
-  //------  restore old settings ---------
-  res = tcsetattr(STDIN_FILENO, TCSANOW, &org_opts);
-  assert(res == 0);
-  return c;
-}
 
 typedef struct {
   long id;
